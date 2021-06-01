7c86894f478afaab999b752b94af63c73067e4de
Facu Lagomarsino <go2facu@gmail.com>
2021-05-31 21:16:07 -0300

    SurfLatinxs

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function myClick() {
+  var element = document.getElementById("menuNav");
+  if(element.classList.contains("visible")){
+    element.classList.remove("visible");
+  }else{
+    element.classList.add("visible");
+  }
+}
+
+
+
+
+
+
+
